a165871f236a7b08ceae7bcf22f52776f11f935f
krishna yadav <yadavsunny7375@gmail.com>
2022-08-16 20:43:29 +0530

    'initial'

---
+++ app.js
@@ -0,0 +1,32 @@
+var express    = require('express');
+var bodyParser = require('body-parser');
+var weather    = require('openweather-apis');
+
+
+//init app
+var app = express();
+
+//set the template engine
+app.set('view engine','ejs');
+
+//fetch the data from request
+app.use(bodyParser.urlencoded({extended:false}));
+
+//default pageload
+app.get('/',function(req,res){
+    res.render('home',{temp:null});
+});
+
+app.post('/',function(req,res){
+    weather.setCity(req.body.city);
+    weather.setAPPID('194092ea340822b15f10d86aa9770915');
+    weather.getAllWeather(function(err,temp){
+        console.log(temp);
+        res.render('home',{temp:temp});
+    });
+});
+
+var port  = process.env.PORT || 3000;
+app.listen(port,function(){
+    console.log('server running at '+port);
+});
